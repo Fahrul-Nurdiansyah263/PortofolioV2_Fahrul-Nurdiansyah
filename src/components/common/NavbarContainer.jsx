731d670aa23a52ada281805ce0b4622e0d1a8b4f
Fahrul-Nurdiansyah263 <nurdiansyahfahrul23@gmail.com>
2026-04-19 12:40:38 +0700

    fix: update CV link in NavbarContainer component

--- src/components/common/NavbarContainer.jsx
+++ src/components/common/NavbarContainer.jsx
@@ -40,7 +40,7 @@ export default function NavbarContainer() {
           <NavItems items={navItems} />
           <div className="flex items-center gap-4">
             <NavbarButton variant="secondary" href="#footer">Hire Me</NavbarButton>
-            <NavbarButton variant="primary" href="https://drive.google.com/file/d/1lBeiV0GKdgIqpGFuoIK3omg7PbtNemUg/view?usp=sharing">CV</NavbarButton>
+            <NavbarButton variant="primary" href="https://drive.google.com/file/d/1bPJ8Aanx9ghahLmaQre_Ip9CM5sWmUyK/view?usp=sharing">CV</NavbarButton>
           </div>
         </NavBody>
         <MobileNav>
@@ -79,7 +79,7 @@ export default function NavbarContainer() {
                 onClick={() => setIsMobileMenuOpen(false)}
                 variant="primary"
                 className="w-full"
-                href="https://drive.google.com/file/d/1lBeiV0GKdgIqpGFuoIK3omg7PbtNemUg/view?usp=sharing"
+                href="https://drive.google.com/file/d/1bPJ8Aanx9ghahLmaQre_Ip9CM5sWmUyK/view?usp=sharing"
               >
                 CV
               </NavbarButton>
